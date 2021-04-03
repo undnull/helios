b97198083f0a91afb289a4d7e36c3dedea48ccf9
undnull <41549137+undnull@users.noreply.github.com>
2021-04-03 19:02:41 +0500

    Update docs yet again. They should work better now!

--- doc/conf.py
+++ doc/conf.py
@@ -4,58 +4,38 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
+# Path setup
+# import os, sys
 # sys.path.insert(0, os.path.abspath("."))
 
-# -- ReadTheDocs -------------------------------------------------------------
-# import os
+# ReadTheDocs
+import os
+if os.environ.get("READTHEDOCS", None) != "True":
+    import sphinx_rtd_theme
+    html_theme = "sphinx_rtd_theme"
+    html_theme_path = [ sphinx_rtd_theme.get_html_theme_path() ]
 
-# if os.environ.get("READTHEDOCS", None) != "True":
-#     import sphinx_rtd_theme
-#     html_theme = "sphinx_rtd_theme"
-#     html_theme_path = [ sphinx_rtd_theme.get_html_theme_path() ]
-
-
-# -- Project information -----------------------------------------------------
-
+# Project information
 project = "Thorn"
 copyright = "2021, Kirill GPRB"
 author = "Kirill GPRB"
 version = "0.0.1"
 
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named "sphinx.ext.*") or your custom
-# ones.
+# General configuration
 extensions = [
-    "sphinxcontrib.fulltoc",
     "sphinx_rtd_theme",
     "myst_parser",
     "breathe",
     "exhale"
 ]
-
-# Add any paths that contain templates here, relative to this directory.
 templates_path = [ "_templates" ]
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = [ "_build", "Thumbs.db", ".DS_Store" ]
 
-# -- Options for Breathe -----------------------------------------------------
+# Options for Breathe
 breathe_projects = { "thorn": "./xml" }
 breathe_default_project = "thorn"
 
-# -- Options for Exhale ------------------------------------------------------
+# Options for Exhale
 exhale_args = {
     "containmentFolder": "./api",
     "rootFileName": "api_reference.rst",
@@ -66,13 +46,18 @@ exhale_args = {
     "exhaleUseDoxyfile": True
 }
 
-# -- Options for domain ------------------------------------------------------
+# Options for domain
 primary_domain = "cpp"
 highlight_language = "cpp"
 
-# -- Options for HTML output -------------------------------------------------
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+# Options for HTML output
+html_show_sourcelink = False
+html_sidebars = {
+    "**": [
+        "globaltoc.html",
+        "relations.html",
+        "sourcelink.html",
+        "searchbox.html"
+    ]
+}
 html_static_path = [ "static" ]
